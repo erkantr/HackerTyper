d0b9b7145f9630b80985fda2eacdf5c2fac6f357
Erkan <erkanm11t@gmail.com>
2022-07-04 12:04:37 +0300

    Initial commit

---
+++ app/src/main/java/com/hacker/MainActivity.java
@@ -0,0 +1,42 @@
+package com.hacker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://hackertyper.net");
+        final ProgressDialog progress = ProgressDialog.show(this, "Lütfen Bekleyin", "Yükleniyor....", true);
+        progress.show();
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                System.out.println("Sayfa Yüklendi");
+                //Toast.makeText(getApplicationContext(), "Sayfa yüklendi", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Bir hata oluştu", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+        });
+    }
+}
